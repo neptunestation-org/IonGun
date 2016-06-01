d0dfdf11ecf847017678f04add985ca7ff2c8f5f
David A. Ventimiglia <dventimi@gmail.com>
2016-06-01 11:10:15 -0700

    Made 'sql' optional

--- src/main/java/org/neptunestation/iongun/util/JDBCURLStreamHandlerFactory.java
+++ src/main/java/org/neptunestation/iongun/util/JDBCURLStreamHandlerFactory.java
@@ -9,6 +9,16 @@ import javax.sql.rowset.*;
 import org.neptunestation.iongun.util.*;
 
 public class JDBCURLStreamHandlerFactory implements URLStreamHandlerFactory {
+    private static Map<List<String>, String> vendors = new HashMap<>();
+    private static Set<String> allVendors = new HashSet<>();
+    static {
+	vendors.put(Arrays.asList("sqlite", "sqlite2"), "sqlite");
+	vendors.put(Arrays.asList("sqlite3"), "sqlite");
+	vendors.put(Arrays.asList("mysql", "mysqls", "mysqlssl"), "mysql");
+	vendors.put(Arrays.asList("oracle", "ora"), "oracle");
+	vendors.put(Arrays.asList("postgresql", "pg", "pgsql", "postgres"), "postgresql");
+	vendors.put(Arrays.asList("postgresqlssl", "pgs", "pgsqlssl", "postgresssl", "pgssl", "postgresqls", "pgsqls", "postgress"), "postgresql");
+	for (List<String> v : vendors.keySet()) allVendors.addAll(v);}
     public String getUrl (URL u, String subname) {
 	return String.format("%s:%s:%s%s", "jdbc", subname,
 			     u.getHost().equals("") ? "" :
@@ -17,7 +27,40 @@ public class JDBCURLStreamHandlerFactory implements URLStreamHandlerFactory {
 			     u.getPath());}
     @Override
     public URLStreamHandler createURLStreamHandler (String protocol) {
-	if (Arrays.asList("sql", "jdbc").contains(protocol))
+	if (allVendors.contains(protocol))
+	    return new URLStreamHandler () {
+		@Override
+		protected URLConnection openConnection (final URL u) throws IOException {
+		    try {
+			return (new URL(String.format("sql:%s:%s%s?%s",
+						      u.getProtocol(),
+						      u.getAuthority()==null ? "" : String.format("//%s", u.getAuthority()),
+						      u.getPath(),
+						      u.getQuery()))).openConnection();}
+		    catch (Exception e) {throw new IOException(e);}}};
+	if ("sql".equals(protocol))
+	    return new URLStreamHandler () {
+		String vendor;
+		protected String getSubname (String vendor) {
+		    for (Map.Entry<List<String>, String> e : vendors.entrySet()) if (e.getKey().contains(vendor)) return e.getValue();
+		    return vendor;}
+		@Override
+		protected void parseURL (URL u, String spec, int start, int end) {
+		    int colon = spec.indexOf(":", start);
+		    vendor = spec.substring(start, colon);
+		    String url = "";
+		    start = colon;
+		    super.parseURL(u, spec, start+1, end);}
+		@Override
+		protected URLConnection openConnection (final URL u) throws IOException {
+		    try {
+			return (new URL(String.format("jdbc:%s:%s%s?%s",
+						      getSubname(vendor),
+						      u.getAuthority()==null ? "" : String.format("//%s", u.getAuthority()),
+						      u.getPath(),
+						      u.getQuery()))).openConnection();}
+		    catch (Exception e) {throw new IOException(e);}}};
+	if ("jdbc".equals(protocol))
 	    return new URLStreamHandler () {
 		String subname;
 		@Override
