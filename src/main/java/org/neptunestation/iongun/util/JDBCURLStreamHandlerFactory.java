db9a8c0c08db5fc254efb7e3cce3be9726c75a38
David A. Ventimiglia <dventimi@gmail.com>
2017-04-17 20:50:58 -0700

    Simplified

--- src/main/java/org/neptunestation/iongun/util/JDBCURLStreamHandlerFactory.java
+++ src/main/java/org/neptunestation/iongun/util/JDBCURLStreamHandlerFactory.java
@@ -95,13 +95,3 @@ public class JDBCURLStreamHandlerFactory implements URLStreamHandlerFactory {
 	for (JDBCURLStreamHandler sh : streamHandlers)
 	    if (sh.accepts(protocol)) return sh;
 	return null;}}
-
-// abstract class DefaultURLStreamHandler extends JDBCURLStreamHandler {
-//     String schemeSpecificPart;
-//     @Override
-//     protected void parseURL (final URL u, final String spec, final int start, final int end) {
-// 	schemeSpecificPart = spec.substring(start);
-// 	super.parseURL(u, spec, start, end);}
-//     @Override
-//     protected URLConnection openConnection (final URL u) throws IOException {
-// 	return (new URL(String.format("%s",schemeSpecificPart))).openConnection();}}
