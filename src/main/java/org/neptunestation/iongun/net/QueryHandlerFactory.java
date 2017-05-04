b4c2629f865441a0821414ddd258a0829c7e4a20
David A. Ventimiglia <dventimi@gmail.com>
2017-05-04 04:33:31 -0700

    Moving the furniture

---
+++ src/main/java/org/neptunestation/iongun/net/QueryHandlerFactory.java
@@ -0,0 +1,17 @@
+package org.neptunestation.iongun.util;
+
+import java.util.*;
+import java.util.concurrent.*;
+
+public abstract class QueryHandlerFactory {
+    private static ServiceLoader<QueryHandler> loader = ServiceLoader.load(QueryHandler.class);
+
+    public static QueryHandler createQueryHandler (String mimeType) {
+	return createQueryHandler(mimeType, new HashMap<String, List<String>>());}
+
+    public static QueryHandler createQueryHandler (String mimeType, Map<String, List<String>> properties) {
+	for (Iterator<QueryHandler> it = loader.iterator(); it.hasNext();) {
+	    QueryHandler h = (QueryHandler)it.next();
+	    h.setProperties(properties);
+	    if (h.accepts(mimeType)) return h;}
+	throw new IllegalStateException("No QueryHandler instances registered");}}
