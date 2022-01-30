8f8aaa3dd8987bd5b3891cc83472bd1c186eac59
David A. Ventimiglia <davidaventimiglia@neptunestation.com>
2022-01-30 06:02:04 -0800

    Killed the BitBucket pipeline config

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,18 +1,4 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((eval . (setenv "JAVA_HOME" "/usr/lib/jvm/java-11-openjdk-amd64"))
-	 (eval . (setenv "CLASSPATH"
-			 (concat
-			  (expand-file-name "/usr/share/java/sqlite-jdbc.jar:")
-			  (let
-			      ((l
-				(dir-locals-find-file
-				 (or
-				  (buffer-file-name)
-				  default-directory))))
-			    (if
-				(listp l)
-				(car l)
-			      l))
-			  "target/classes"))))))
+((nil . ((eval . (setenv "JAVA_HOME" "/usr/lib/jvm/java-11-openjdk-amd64")))))
